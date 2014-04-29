750a37bc129ae536006ea7cbe5e20c3686423ddd
feelobot <felix.a.rod@gmail.com>
2014-04-28 22:47:38 -0700

    competition pages up

--- api/controllers/CompetitionController.js
+++ api/controllers/CompetitionController.js
@@ -15,7 +15,11 @@ module.exports = {
     res.render('main/new');
   },
   index: function (req,res) {
-    res.render('main/competition');
+    var id = req.param('id');
+    Competition.findOne({id: id}, function (err, competition) {
+       if (err) return res.send(err,500);
+       res.render('main/competition',{ competition: competition });
+    });
   },
 
   _config: {}
